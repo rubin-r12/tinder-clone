7c611f01fc62d0e315fa09bfc7a85cfc04281c26
Rubin <rubinradhakrishnan@gmail.com>
2020-08-29 15:57:19 +0530

    Add logo and buttons in the Header

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a Tinder Clone🚀</h1>
+      <Header />
+      {/* Tinder cards */}
+      {/* Buttons below tinder cards */}
+
+      {/* Chats screeen */}
+      {/* Individual chat screen */}
     </div>
   );
 }
