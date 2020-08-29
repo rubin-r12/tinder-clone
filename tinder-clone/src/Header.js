b0c1cc381d75e0a88aea7d06fe50d4427d246d68
Rubin <rubinradhakrishnan@gmail.com>
2020-08-29 23:51:34 +0530

    Redirect the back button from the chats section to the home page

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -4,13 +4,16 @@ import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 function Header({ backButton }) {
+  const history = useHistory();
   return (
     <div className="header">
       {backButton ? (
-        <ArrowBackIosIcon fontSize="large" className="header__icon" />
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon fontSize="large" className="header__icon" />
+        </IconButton>
       ) : (
         <IconButton>
           <PersonIcon className="header__icon" fontSize="large" />
