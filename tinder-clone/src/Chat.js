ea0fcf66b09809d7646625fa78d9b3276b35f85a
Rubin <rubinradhakrishnan@gmail.com>
2020-08-30 12:27:36 +0530

    Add props to the chat components

--- tinder-clone/src/Chat.js
+++ tinder-clone/src/Chat.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Chat.css";
 import Avatar from "@material-ui/core/Avatar";
 
 function Chat({ name, message, profilePic, timestamp }) {
   return (
     <div className="chat">
-      <Avatar className="chat__image" alt={name} src={profilePic} />
+      <Avatar className="chat__image" src={profilePic} />
+      <div className="chat__details">
+        <h2>{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p className="chat__timestamp">{timestamp}</p>
     </div>
   );
 }
