d10641fcf929133fe9f0bc456219398b7791b3dc
Rubin <rubinradhakrishnan@gmail.com>
2020-08-30 12:17:43 +0530

    Add chat component within the Chats component

--- tinder-clone/src/Chats.js
+++ tinder-clone/src/Chats.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Chats.css";
+import Chat from "./Chat.js";
 
 function Chats() {
   return (
-    <div>
-      <h1>Welcome to teh chats section</h1>
+    <div className="chats">
+      <Chat
+        name="Mark"
+        message="Yo whats up"
+        timestamp="40 seconds ago"
+        profilePic="https://image.cnbcfm.com/api/v1/image/104410446-GettyImages-669889288.jpg?v=1529474846&w=1400&h=950"
+      />
     </div>
   );
 }
