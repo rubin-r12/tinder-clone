aaedc815e5a06a68fb3585d66c0a3327dadc7bff
Rubin <rubinradhakrishnan@gmail.com>
2020-08-30 14:15:57 +0530

    Pop the messages in the Chat Screen component

--- tinder-clone/src/ChatScreen.js
+++ tinder-clone/src/ChatScreen.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ChatScreen.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function ChatScreen() {
+  const [messages, setMessages] = useState([
+    {
+      name: "Mark",
+      image:
+        "https://image.cnbcfm.com/api/v1/image/104410446-GettyImages-669889288.jpg?v=1529474846&w=1400&h=950",
+      message: "Whats up",
+    },
+    {
+      name: "Mark",
+      image:
+        "https://image.cnbcfm.com/api/v1/image/104410446-GettyImages-669889288.jpg?v=1529474846&w=1400&h=950",
+      message: "Good morning",
+    },
+    {
+      message: "Hey, how are you Mark",
+    },
+  ]);
+
   return (
     <div className="chatScreen">
-      <h1>It Works</h1>
+      <p>You matched with Mark on 30/08/20</p>
+      {messages.map((message) => (
+        <div className="chatScreen__message">
+          <Avatar
+            className="chatScreen__image"
+            alt={message.name}
+            src={message.image}
+          />
+          <p className="chatScreen__text">{message.message}</p>
+        </div>
+      ))}
     </div>
   );
 }
